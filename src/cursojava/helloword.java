9d16818e2e353f818ed48e2f62c276c7d622e595
Isaias Caetano <131115372+isaiasscaetano@users.noreply.github.com>
2024-04-22 00:26:18 -0300

    Add files via upload

--- src/cursojava/helloword.java
+++ src/cursojava/helloword.java
@@ -1,2 +1,7 @@
-package cursojava;public class helloword {
+package cursojava;
+
+public class helloword {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+    }
 }
