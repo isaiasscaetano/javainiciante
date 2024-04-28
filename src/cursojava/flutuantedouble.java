972aea1b4cddadd005047174d9cbde0b3a09a58e
Isaias Caetano <131115372+isaiasscaetano@users.noreply.github.com>
2024-04-28 14:08:58 -0300

    Add files via upload

---
+++ src/cursojava/flutuantedouble.java
@@ -0,0 +1,17 @@
+package cursojava;
+
+public class flutuantedouble {
+   public static void main(String[] args){
+       double hora = 21.38;
+       System.out.println("agora sao " + hora + " Horas ");
+
+       double conta = 2.4/2;
+      System.out.println(conta);
+
+      int conta2 = 10 / 5;
+      System.out.println(conta2);
+
+      double conta3 = 10 / 5;
+      System.out.println(conta3);
+   }
+}
---
+++ src/cursojava/lidandocomcaractereseString.java
@@ -0,0 +1,20 @@
+package cursojava;
+
+public class lidandocomcaractereseString {
+    public static void main(String[] args){
+        char letra = 'a';
+        System.out.println(letra);
+
+        char valor = 66;
+        System.out.println(valor);
+
+        valor = (char) (valor + 1);
+        System.out.println(valor);
+
+        String mensagem = "eu estou aprendendo java";
+        System.out.println(mensagem);
+
+        mensagem = mensagem + "com isaias";
+        System.out.println(mensagem);
+    }
+}
